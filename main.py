4accc71ffdb83966ec4fff6d2a5e8f3e644847ba
Lorenzo <lorenzodr95@hotmail.com>
2021-07-31 19:11:43 -0300

    Making script

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # -*- encoding = utf-8 -*-
 
 import xlrd
+from os import listdir
+from os.path import isfile, join
+
 
 def atualizar_posicao():
-    pass
+    print(listdir('./paste-sheets/'))
 
 
 if __name__ == '__main__':
